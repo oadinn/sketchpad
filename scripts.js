4b16eb8c97c70ebce1d3785bc6bc9cf11b8cabe0
oadinn <duynguyen@Duys-Air.fios-router.home>
2024-05-14 17:32:21 -0400

    add color picker, supports clear button and changing of grid density

--- scripts.js
+++ scripts.js
@@ -12,19 +12,33 @@ gridContainer.style.height = `${GRIDSIZE}px`
 function changeBackgroundColor () {
     this.style.backgroundColor = currentColor;
 }
+const renderGrid = () => {
+    for (let i = 0; i < rows * cols; i++) {
+        const gridCell = document.createElement('div')
+        gridCell.style.width = `${GRIDSIZE / rows - 2}px`
+        gridCell.style.height = `${GRIDSIZE / cols - 2}px`
+        gridCell.classList.add('grid-cell')
+        gridContainer.appendChild(gridCell)
+        gridCell.addEventListener('mouseover', changeBackgroundColor)
+    }
+}
 
-for (let i = 0; i < rows * cols; i++) {
-    const gridCell = document.createElement('div')
-    gridCell.style.width = `${GRIDSIZE / rows - 2}px`
-    gridCell.style.height = `${GRIDSIZE / cols - 2}px`
-    gridCell.classList.add('grid-cell')
-    gridContainer.appendChild(gridCell)
-    gridCell.addEventListener('mouseover', changeBackgroundColor)
+const deleteCurrentGrid = () => {
+    gridContainer.textContent = "";
+}
+
+changeGridSize = (size) => {
+    rows = size
+    cols = size
+    deleteCurrentGrid()
+    renderGrid()
 }
 
 const activateEraser = () => {
     currentColor = "white"
 }
+
+
 
 const activateRandom = () => {
     while (currentColor = 'random') {
@@ -32,7 +46,23 @@ const activateRandom = () => {
     }
 }
 
+const activateClear = () => {
+    deleteCurrentGrid()
+    renderGrid()
+}
+
 
 // query selectors
 document.getElementById('eraser').addEventListener('click', () => activateEraser())
+document.getElementById('colorPicker').addEventListener('change', function() {
+    currentColor = this.value;
+});
+document.getElementById('clear').addEventListener('click', () => activateClear())
+document.getElementById('16').addEventListener('click', () => changeGridSize(16))
+document.getElementById('32').addEventListener('click', () => changeGridSize(32))
+document.getElementById('64').addEventListener('click', () => changeGridSize(64))
+document.getElementById('100').addEventListener('click', () => changeGridSize(100))
+
+
+renderGrid()
 
